839a0686e88633a955e08a7fadc9c010e70233bb
shraddhac14 <2016shraddha.chavan@ves.ac.in>
2018-07-21 23:15:51 +0530

    homeactivty
    java

--- Annadhan/app/src/main/java/com/example/lenovo/annadhan/HomeActivity.java
+++ Annadhan/app/src/main/java/com/example/lenovo/annadhan/HomeActivity.java
@@ -1,13 +1,34 @@
 package com.example.lenovo.annadhan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 
 public class HomeActivity extends AppCompatActivity {
-
+Button btnDonate,btnVolunteer;
+HorizontalScrollView slider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnDonate=(Button)findViewById(R.id.btnDonate);
+        btnVolunteer=(Button)findViewById(R.id.btnVolunteer);
+        slider=(HorizontalScrollView)findViewById(R.id.slider);
+
+        btnDonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(HomeActivity.this,FooduploadActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
